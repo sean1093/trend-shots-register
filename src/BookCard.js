894c90985cc9ba7c08cb010c60dcb1df201c5a00
sean-chou <sean_chou@trendmicro.com>
2021-12-12 23:47:20 +0800

    feat: UX review style and wording change

--- src/BookCard.js
+++ src/BookCard.js
@@ -9,21 +9,40 @@ const BookCard = () => {
   return (
     <div className="card">
       <div className="card-header">
-        <p className="card-title">
+        <p className="card-title bold">
           {CONTENT.content.title}
         </p>
-        <p className="card-content">
-          {CONTENT.content.date}
-        </p>
-        <p className="card-content">
-          {CONTENT.content.location}
-        </p>
-        <p className="card-content">
-          {CONTENT.content.info}
-        </p>
-        <p className="card-content">
-          {CONTENT.content.note}
-        </p>
+        <div className="card-section">
+          <p className="card-content">
+            {CONTENT.content.date}
+          </p>
+          <p className="card-content">
+            {CONTENT.content.location}
+          </p>
+          <p className="card-content">
+            {CONTENT.content.fee}
+          </p>
+          <p className="card-content bold">
+            {CONTENT.content.action}
+          </p>
+        </div>
+        <div className="card-section">
+          <p className="card-content">
+            {CONTENT.content.date_en}
+          </p>
+          <p className="card-content">
+            {CONTENT.content.location_en}
+          </p>
+          <p className="card-content">
+            {CONTENT.content.fee_en}
+          </p>
+          <p className="card-content bold">
+            {CONTENT.content.action_en}
+          </p>
+          <div className="note">
+            {CONTENT.content.note}
+          </div>
+        </div>
       </div>
       <div className="card-body">
         <button onClick={openUrl}>
